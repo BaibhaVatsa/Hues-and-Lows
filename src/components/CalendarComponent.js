960f496d0027cbfcaeb65f219fb5aa1f80242324
Baibhav Vatsa <baibhavvatsa@gmail.com>
2019-11-02 08:08:23 -0500

    calendar component should be fine now

---
+++ src/components/CalendarComponent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+
+class CalendarComponent extends React.Component {
+    state = {
+        date: new Date()
+    }
+
+    passOnChange = (newDate) => {
+        this.setState({
+            date: newDate
+        });
+        this.props.dateChosen.bind(this, this.state.date);
+    }
+
+    render() {
+        return (
+            <div>
+                <Calendar onChange={this.passOnChange} value={this.props.date}/>
+            </div>
+        );
+    }
+}
+
+export default CalendarComponent;
