deaf1fe319423698fc77ded38e37100102d3085b
Baibhav Vatsa <baibhavvatsa@gmail.com>
2019-11-02 10:06:35 -0500

    won't build without these

---
+++ src/Constants.js
@@ -0,0 +1,12 @@
+// import Emotion from '../backend/models/Emotion'
+
+// const Happy = new Emotion();
+// const Sad = new Emotion();
+// const Anxious = new Emotion();
+// const Angry = new Emotion();
+// const Frustrated = new Emotion();
+
+// const AllEmotions = [Happy, Sad, Anxious, Angry, Frustrated];
+const AllEmotions = ["blue", "white", "orange", "red", "grey"];
+
+export default AllEmotions;
