eeb39c7bdd95e9c2dddbe0255c55398354878023
OAkala <olalekan56@gmail.com>
2019-11-02 01:56:36 -0500

    Completed basics for AddButtonComponent

---
+++ src/components/AddButtonComponent.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import { makeStyles } from '@material-ui/core/styles';
+
+class AddButton extends React.Component {
+    constructor() {
+        super();
+        this.classes = makeStyles(theme => ({
+            fab: {
+                margin: theme.spacing(1),
+            },
+            extendedIcon: {
+                marginRight: theme.spacing(1),
+            },
+        }));
+        this.isClicked = false;
+    }
+
+    clicked() {
+        this.setState({
+            isClicked: true,
+        })
+    }
+
+    render() {
+
+        return (
+            <Fab
+                color="primary" size="small" aria-label="add"
+                className={this.classes.fab}
+
+                onClick={() => this.clicked()}>
+                <AddIcon />
+            </Fab >
+
+        );
+    }
+
+}
+
+export default AddButton;
