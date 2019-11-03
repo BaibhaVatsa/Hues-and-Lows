e7e3cbb14d50cdfe647531a19d89bd347619c3ae
OAkala <olalekan56@gmail.com>
2019-11-03 00:49:59 -0500

    Fixed

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import CancelButtonComponent from '../components/CancelButtonComponent';
 import ExtraDataInputComponent from '../components/ExtraDataInputComponent';
 import MoodInputComponent from '../components/MoodInputComponent';
 import NotesInputComponent from '../components/NotesInputComponent';
@@ -9,7 +8,6 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Grid from '@material-ui/core/Grid';
@@ -96,9 +94,9 @@ export default class InputView extends React.Component {
                             justify="center"
                             alignItems="flex-start"
                         >
-                            <Grid item>
+                            {/* <Grid item>
                                 <MoodInputComponent />
-                            </Grid>
+                            </Grid> */}
                             <Grid item xs={12}>
                                 {this.displayNotes()}
                             </Grid>
