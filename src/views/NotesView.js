c87e53713f6c2cd6212352a91873c796191e2827
Charles Wang <charleswang@Charless-MacBook-Air.local>
2019-11-02 22:12:56 -0500

    Notes View

--- src/views/NotesView.js
+++ src/views/NotesView.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import ListViewNoteComponent from '../components/ListViewNoteComponent';
+import ListViewNoteComponent from '../components/ListViewNoteComponent';
 
 export default class NotesView extends React.Component {
     render() {
         return (
             <div>
-               
+               <ListViewNoteComponent></ListViewNoteComponent>
             </div>
         );
     }
