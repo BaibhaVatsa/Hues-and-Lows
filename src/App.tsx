524c7b1921dcd298cb5c4c571e9e5c5aeafa5b9d
Kabindra Shrestha <kabindra.shrestha@vanderbilt.edu>
2019-12-19 10:40:09 -0600

    date component

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,43 @@
-import React, { useState, Suspense } from 'react';
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom';
-import './App.css';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, Suspense } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import "./App.css";
+import * as serviceWorker from "./serviceWorker";
 // import CalendarView from './views/CalendarView';
-import HomeView from './views/HomeView';
+import HomeView from "./views/HomeView";
 // import NotesView from './views/NotesView';
 // import InputView from './views/InputView';
-import WelcomeView from './views/WelcomeView';
-
+import WelcomeView from "./views/WelcomeView";
 
 const App: React.FC = () => {
-  const [date, setDate] = useState(Date());
+  const [date, setDate] = useState(new Date());
 
   return (
     <BrowserRouter>
-      <div className='App'>
+      <div className="App">
         <Suspense fallback={<WelcomeView />}>
           <Switch>
-            <Route path='/home'>
-              <HomeView />
+            <Route path="/home">
+              <HomeView date={date} />
             </Route>
-            <Route path='/notes'>
+            <Route path="/notes">
               {/* TODO directly open popup for Notes */}
               {/* <NotesView /> */}
             </Route>
-            <Route path='/calendar'>
+            <Route path="/calendar">
               {/* TODO directly open popup for Calendar */}
               {/* <CalendarView /> */}
             </Route>
-            <Route path='/input'>
+            <Route path="/input">
               {/* TODO directly open popup for Input */}
               {/* <InputView /> */}
             </Route>
-            <Redirect from='*' to='/home' />
+            <Redirect from="*" to="/home" />
           </Switch>
         </Suspense>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
 
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import PinWheelComponent from '../components/PinWheelComponent';
+import React from "react";
+import PinWheelComponent from "../components/PinWheelComponent";
+import DateComponent from "../components/DateComponent";
 
 type HomeViewProps = {
-
-}
+  date: Date;
+};
 
 const HomeView: React.FC<HomeViewProps> = (props: HomeViewProps) => {
-    return (
-        <PinWheelComponent handleClick={() => alert('clicked!')} colours={['#d0c9d6', '#000000', '#787878']}></PinWheelComponent>
-    );
-}
+  return (
+    <div>
+      <DateComponent date={props.date}></DateComponent>
+
+      <PinWheelComponent
+        handleClick={() => alert("clicked!")}
+        colours={["#d0c9d6", "#000000", "#787878"]}
+      ></PinWheelComponent>
+    </div>
+  );
+};
 
 export default HomeView;
